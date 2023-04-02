c2b6ec97acf326ca4d5d2e0f238ed323d556c513
Lighter888 <mishcenkomv@gmail.com>
2023-04-01 19:06:57 -0700

    The task 6 is completed.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random random = new Random();
+        int numberTask = random.nextInt(80);
+        System.out.println(numberTask);
+
+
     }
 }
