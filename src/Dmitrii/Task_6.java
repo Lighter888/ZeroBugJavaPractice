270624773dccfb80ec179eeba0ccb366db0923bf
alenb <dmitriiblum@gmail.com>
2023-04-04 21:55:04 -0400

    Just for practice from Dmitry

---
+++ src/Dmitrii/Task_6.java
@@ -0,0 +1,27 @@
+package Dmitrii;
+
+/*
+String h="AAABBBCCC";
+        String result = "";
+
+        output
+        ABC
+
+ */
+
+public class Task_6 {
+
+    public static void main(String[] args) {
+
+        String h = "AAAAFBBBCCC";
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < h.length(); i++) {
+            if (!result.toString().contains("" + h.charAt(i))) {
+                result.append(h.charAt(i));
+            }
+        }
+        System.out.println(result);
+        System.out.println("11111");
+    }
+}
