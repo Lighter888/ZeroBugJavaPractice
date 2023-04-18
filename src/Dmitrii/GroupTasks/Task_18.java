08af0a5a50fa01358ac101850f6179efef19e6cc
alenb <dmitriiblum@gmail.com>
2023-04-17 21:44:59 -0400

    New task 18 was added

---
+++ src/Dmitrii/GroupTasks/Task_18.java
@@ -0,0 +1,24 @@
+package Dmitrii.GroupTasks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task_18 {
+
+    public static void main(String[] args){
+
+          String divisibleByNum ="";
+
+        Scanner scan = new Scanner(System.in);
+        int division = scan.nextInt();
+
+        int[] arr = new int[100];
+        for (int i=0; i<arr.length; i++){
+            arr[i] = i+1;
+            if(arr[i]%division==0){
+                divisibleByNum +=  arr[i] + ", ";
+            }
+        }
+        System.out.println(divisibleByNum);
+    }
+}
