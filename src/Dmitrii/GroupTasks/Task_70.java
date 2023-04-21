21cb1935756366679f06864b0fe25f89b06d2eaa
alenb <dmitriiblum@gmail.com>
2023-04-20 22:19:00 -0400

    New solved task 70

---
+++ src/Dmitrii/GroupTasks/Task_70.java
@@ -0,0 +1,32 @@
+package Dmitrii.GroupTasks;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Task_70 {
+
+    public static void main(String[] args) {
+
+//        //70 Store into Array
+//        int a[]={1,3,5};
+//        int b[]={2,4,6};
+//        output
+//                [1,2,3,4,5,6]
+
+        int[] a = {1, 3, 5};
+        int[] b = {2, 4, 6};
+        ArrayList<Integer> combinedInt = new ArrayList<>();
+        // First for loop iterate array "a" and add every each element of an array to the ArrayList
+        for (int i = 0; i < a.length; i++) {
+            combinedInt.add(a[i]);
+        }
+        // Second for loop iterate array "b" and add every each element of an array to the ArrayList
+        for (int i = 0; i < b.length; i++) {
+            combinedInt.add(b[i]);
+        }
+        // Sort all elements of an ArrayList uses sorted method
+        System.out.println(Arrays.toString(combinedInt.stream().sorted().toArray()));
+    }
+}
