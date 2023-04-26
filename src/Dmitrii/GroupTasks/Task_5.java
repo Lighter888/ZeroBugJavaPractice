dfb78376e30b07bcce213d581fe9dc081a21dba3
alenb <dmitriiblum@gmail.com>
2023-04-26 19:33:03 -0400

    Added new solution with Array

--- src/Dmitrii/GroupTasks/Task_5.java
+++ src/Dmitrii/GroupTasks/Task_5.java
@@ -20,8 +20,9 @@ public class Task_5 {
 //    ArrayList<Integer> list = new ArrayList();
     public static void main (String[] args){
 
-        ascendingOrder ();
+        ascendingOrder();
         descendingOrder();
+        sortArray();
     }
 
     public static void ascendingOrder (){
@@ -56,4 +57,23 @@ public class Task_5 {
         System.out.println(arrList);
     }
 
-}
+    public static void sortArray (){
+        int [] arr = {1, 2, 3, 49, 6, 5};
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int element : arr){
+            list.add(element);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                Integer intI = list.get(i);
+                Integer intJ = list.get(j);
+                if (intI > intJ){
+                    list.set(i, intJ);
+                    list.set(j,intI );
+                }
+            }
+        }
+        System.out.println(list);
+       }
+    }
+
