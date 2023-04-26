367c2b47ff87e70822b4072af3700ff2f1a981a7
Dmitrii Aleinikov <107445568+DmitriiAleinikov@users.noreply.github.com>
2023-04-26 19:07:17 -0400

    Change name of methods and add one more solution (#18)

--- src/Dmitrii/GroupTasks/Task_5.java
+++ src/Dmitrii/GroupTasks/Task_5.java
@@ -19,10 +19,12 @@ public class Task_5 {
 //2int[] arr = {1, 2, 3, 49, 6, 5};
 //    ArrayList<Integer> list = new ArrayList();
     public static void main (String[] args){
-        solutionTaskFive ();
+
+        ascendingOrder ();
+        descendingOrder();
     }
 
-    public static void solutionTaskFive(){
+    public static void ascendingOrder (){
         ArrayList<Integer> arrList = new ArrayList<>(Arrays.asList(1, 10, 50, 2, 4, 50, 10, 3, 2,5, 4));
 
         for (int i = 0; i < arrList.size(); i++) {
@@ -30,6 +32,22 @@ public class Task_5 {
                 Integer intI = arrList.get(i);
                 Integer intJ = arrList.get(j);
                 if (intI > intJ){
+                    arrList.set(i, intJ);
+                    arrList.set(j,intI );
+                }
+            }
+        }
+        System.out.println(arrList);
+    }
+
+    public static void descendingOrder(){
+        ArrayList<Integer> arrList = new ArrayList<>(Arrays.asList(1, 10, 50, 2, 4, 50, 10, 3, 2,5, 4));
+
+        for (int i = 0; i < arrList.size(); i++) {
+            for (int j = i+1; j < arrList.size(); j++) {
+                Integer intI = arrList.get(i);
+                Integer intJ = arrList.get(j);
+                if (intI < intJ){
                     arrList.set(i, intJ);
                     arrList.set(j,intI );
                 }
