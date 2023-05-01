e08a71668ad0212c28fa82a9860c815aa0dd9646
Dmitrii Aleinikov <107445568+DmitriiAleinikov@users.noreply.github.com>
2023-04-30 21:42:22 -0400

    Rebuild task 5,  and Added group task 5(Tania Solution) (#23)
    * New solution of task 35 is added
    
    * New solution of task 22 is added
    
    * Rebuild task 5,
    and Added group task 5(Tania Solution)

---
+++ src/Dmitrii/ImportantFeatures.java
@@ -0,0 +1,27 @@
+package Dmitrii;
+
+import java.time.Duration;
+
+public class ImportantFeatures {
+
+//    @Test
+//    public void waiters (){
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//
+////        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("landscape")));
+////        driver.findElement(By.id("landscape"));
+////        Assert.assertTrue(element.getAttribute("src").contains("landscape"));
+//
+//        driver.findElement(By.xpath("//span[text()='7']")).click();
+//        driver.findElement(By.xpath("//span[text()='+']")).click();
+//        driver.findElement(By.xpath("//span[text()='7']")).click();
+//        driver.findElement(By.xpath("//span[text()='=']")).click();
+//        boolean testEquals = wait.until(ExpectedConditions.textToBe(By.xpath("//div[@class='screen']"), "14"));
+//        Assert.assertTrue(testEquals);
+//        driver.quit();
+//    }
+
+}
