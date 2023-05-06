a612ce59fcacdd142cc1049cf6f0b33b5b393eb7
Dmitrii Aleinikov <107445568+DmitriiAleinikov@users.noreply.github.com>
2023-05-06 15:26:16 -0400

    Solution of task 54 added (#24)
    * New solution of task 35 is added
    
    * New solution of task 22 is added
    
    * Rebuild task 5,
    and Added group task 5(Tania Solution)
    
    * Added task 54
    
    * Added task 54

---
+++ src/Dmitrii/GroupTasks/Task_54.java
@@ -0,0 +1,28 @@
+package Dmitrii.GroupTasks;
+
+import java.util.Scanner;
+
+public class Task_54 {
+
+//    //54
+//    Ask user enter 5 words and declare long word or Array
+//    String h[]={"hellow", "whyyyyyyyyyy", "byererdddd", "apple" , "notettttt"};
+//   ************ Looks like someone deleted end part of the task description!!!***********
+//    It's suppose to be "found larger word from Array
+
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter 5 words: ");
+        System.out.println(largerWord(new String[]{scan.next(), scan.next(), scan.next(), scan.next(), scan.next()}));
+    }
+
+    public static String largerWord (String [] arrStr){
+        String maxWord = "";
+        for (int i = 0; i < arrStr.length; i++){
+            if(arrStr[i].length() > maxWord.length()){
+                maxWord = arrStr[i];
+            }
+        }
+        return maxWord;
+    }
+}
