047896e8b7e6a07e271aad79b7f6f52499602a29
Max Mish <mishcenkomv@gmail.com>
2023-05-07 10:56:51 -0700

    Done tasks 24,25

---
+++ src/Maksim/Task_25.java
@@ -0,0 +1,25 @@
+package Maksim;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task_25 {
+
+//    USE ITERATOR
+//  ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,101,200,300));
+//
+//  output
+//  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    public static List<Integer> sortListLess10(List<Integer> list){
+        list.removeIf(num -> (num >= 10));
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4,45,23,5,6,7,8,9,101,200,300));
+        System.out.println(sortListLess10(list1));
+    }
+
+}
