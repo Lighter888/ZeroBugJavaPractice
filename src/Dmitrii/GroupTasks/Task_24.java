a5900b124463b319981b2f6058929465ca83c415
Dmitrii Aleinikov <107445568+DmitriiAleinikov@users.noreply.github.com>
2023-05-09 21:53:49 -0400

    New solution added to Dmitrii folder (#26)
    * JavaQuiz Overriden  answer method
    Added one more abstract method into Quiz interface
    
    * New solution of Tasks 24 added to Dmitrii folder

---
+++ src/Dmitrii/GroupTasks/Task_24.java
@@ -0,0 +1,26 @@
+package Dmitrii.GroupTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Task_24 {
+
+//    //24  USE ITERATOR
+//    ArrayList -- Remove "Ahmed"
+//    ArrayList<String> names = new ArrayList<>(Arrays.asList("Ahmed", "John", Eric", "Ahmed".));
+
+    public static void main(String[] args) {
+        ArrayList<String> namesList = new ArrayList<>(Arrays.asList("Ahmed", "John", "Eric", "Ahmed"));
+        System.out.println(iteratorRemove(namesList));
+    }
+    public static ArrayList<String> iteratorRemove(ArrayList<String> names){
+        Iterator<String> iterator = names.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals("Ahmed")) {
+                iterator.remove();
+            }
+        }
+        return names;
+    }
+}
