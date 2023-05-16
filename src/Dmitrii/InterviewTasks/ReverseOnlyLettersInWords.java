5d4393d92be639f2134d51267f1a0613e6d284a5
alenb <dmitriiblum@gmail.com>
2023-05-16 12:20:15 -0400

    New solution added of Revers Only Letters into the words

---
+++ src/Dmitrii/InterviewTasks/ReverseOnlyLettersInWords.java
@@ -0,0 +1,21 @@
+package Dmitrii.InterviewTasks;
+
+import java.awt.desktop.PreferencesEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReverseOnlyLettersInWords {
+     public static void main(String[] args){
+         String inputStr = "Hello students school";
+         System.out.println(reversedWordsOfStr(inputStr));
+     }
+
+     public static String reversedWordsOfStr(String str){
+         String reversedWordsStr = "";
+         String [] arrStr = str.split(" ");
+         for (int i = 0; i < arrStr.length; i++) {
+             reversedWordsStr += new StringBuilder(arrStr[i]).reverse().toString() + " ";
+         }
+         return reversedWordsStr.strip();
+     }
+}
