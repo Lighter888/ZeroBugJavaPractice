41830c5213b971c9eaaa9cd558caaaf616c65da0
alenb <dmitriiblum@gmail.com>
2023-05-15 22:36:23 -0400

    Added new Task 21 Factorial Number And Triangle out of slashes

---
+++ src/Dmitrii/GroupTasks/Task_21.java
@@ -0,0 +1,16 @@
+package Dmitrii.GroupTasks;
+
+public class Task_21 {
+    public static void main(String[] args){
+//        4! = 4 × 3 × 2 × 1 = 24
+//        7! = 7 × 6 × 5 × 4 × 3 × 2 × 1 = 5040
+//        1! = 1
+
+        int num = 4;
+        int sum = 1;
+        for(int i=1; i<=num; i++){
+            sum = sum*i;
+        }
+        System.out.println(sum);
+    }
+}
