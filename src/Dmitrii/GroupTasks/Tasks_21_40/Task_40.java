c5f92efa9850662069d8db09eb7f251b5d6b81e6
alenb <dmitriiblum@gmail.com>
2023-05-18 21:16:30 -0400

    !!!task 40

---
+++ src/Dmitrii/GroupTasks/Tasks_21_40/Task_40.java
@@ -0,0 +1,24 @@
+package Dmitrii.GroupTasks.Tasks_21_40;
+
+import java.util.ArrayList;
+
+public class Task_40 {
+//    //40  FIND missing number
+//
+//    int r[]={-1,3,5,6,7,9,13,18,70,80,1400,1401,1405};
+    public static void main(String[] args){
+        ArrayList<Integer> missedNum = new ArrayList<>();
+        int arr [] = {-1,3,5,6,7,9,13,18,70,80,1400,1401,1405};
+        for(int i = 0; i < arr.length; i++){
+            if(i==arr.length-1){
+                break;
+            }  else if(arr[i]+1 != arr[i+1]){
+                int iteration = arr[i+1]-arr[i];
+                for(int j=1; j < iteration; j++){
+                    missedNum.add(arr[i]+j);
+                }
+            }
+        }
+        System.out.println(missedNum);
+    }
+}
--- src/Dmitrii/InterviewTasks/ReversString.java
+++ src/Dmitrii/InterviewTasks/ReversString.java
@@ -1,39 +1,63 @@
 package Dmitrii.InterviewTasks;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class ReversString {
 
     public static void main(String[] args) {
         String string = "Hello World";
-        char [] charStr = string.toCharArray();
+        String input = "Geeks For Geeks";
+        char[] charStr = string.toCharArray();
         System.out.println("Trough For Loop: " + reverseStrForLoop(string));
         System.out.println("Trough String Builder: " + reverseStrBuilder(string));
-        System.out.println("Uses char Array: "  + reversStrCharArr(charStr));
+        System.out.println("Uses char Array: " + reversStrCharArr(charStr));
+        System.out.println("Uses Arraylist: " + reverseStrList(input));
+        System.out.println("Uses Another Way: " + anotherWayReversString(string));
 
     }
 
-    public static String reverseStrForLoop(String str){
+    public static String reverseStrForLoop(String str) {
         String reversedStr = "";
-        for (int i = str.length()-1; i >= 0; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             reversedStr += str.charAt(i);
         }
         return reversedStr;
     }
 
-    public static String reverseStrBuilder (String str){
+    public static String reverseStrBuilder(String str) {
         String reversedStr = new StringBuilder(str).reverse().toString();
         return reversedStr;
     }
 
-    public static String reversStrCharArr (char[] letters){
+    public static String reversStrCharArr(char[] letters) {
         StringBuilder reverseStr = new StringBuilder();
-        for(int i = letters.length-1; i >= 0; i--){
+        for (int i = letters.length - 1; i >= 0; i--) {
             reverseStr.append(letters[i]);
         }
         return reverseStr.toString();
     }
 
+    public static String reverseStrList(String str) {
+        List<Character> arrList = new ArrayList<>();
+        for (Character character : str.toCharArray()) {
+            arrList.add(character);
+        }
+        Collections.reverse(arrList);
+        return arrList.toString().replace(", ", "").replace("[","").replace("]","");
+    }
 
-
+    public static String anotherWayReversString(String str){
+        List<Character> arrList = new ArrayList<>();
+        for (Character character : str.toCharArray()) {
+            arrList.add(character);
+        }
+        String revers = arrList.stream()
+                               .map(String::valueOf)
+                               .collect(Collectors.joining());
+        return revers;
+    }
 }
