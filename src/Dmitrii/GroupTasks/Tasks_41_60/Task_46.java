b62cb2b76bddf67d4410e556704ae3e2c1103543
alenb <dmitriiblum@gmail.com>
2023-05-24 20:57:02 -0400

    Upgraded Merdiven - removed percentages

--- src/Dmitrii/GroupTasks/Tasks_41_60/Task_46.java
+++ src/Dmitrii/GroupTasks/Tasks_41_60/Task_46.java
@@ -10,63 +10,57 @@ public class Task_46 {
 
     public static void main(String[] args) {
 
-//        // Solution 1
-////        String str = "aaa10ss20bb30";
-////  //    output = 60
-////        int sum = 0;
-////        String newStr = "";
-////        for (int i = 0; i < str.length(); i++) {
-////            if (Character.isDigit(str.charAt(i))) {
-////                newStr += str.charAt(i);
-////            } else {
-////                newStr += " ";
-////            }
-////        }
-////        System.out.println(newStr);
-////        newStr = newStr.trim();
-////        String[] arrStr = newStr.split(" ");
-////        System.out.println(Arrays.toString(arrStr));
-////        for (String s : arrStr) {
-////            if (!s.equals("")) {
-////                sum += Integer.parseInt(s);
-////            }
-////        }
-////        System.out.println(sum);
-////
-//////        // Solution 2
-//////        String s = "aaa10ss20bb30";
-//////        int sum = 0;
-//////        String[] numbers = s.split("[^0-9]+");
-//////        for (String number : numbers) {
-//////            sum += Integer.parseInt(number);
-//////        }
-//////        System.out.println("Sum of numbers: " + sum);
-
+        //  Main group solution
         String str = "aa10aa%^^$#%^$a10ss20bb30aww1tt1";
-        String digits;
-
-        digits = str.replaceAll("[^0-9]+"," ");
-        System.out.println(digits);
-
-
-//        String digits = "";
-//        for (int i = 0; i < str.length(); i++) {
-//            if (Character.isDigit(str.charAt(i))) {
-//                digits += str.charAt(i);
-//            } else {
-//                digits += " ";
-//            }
-//        }
         int sum = 0;
-
-        String[] nums = digits.trim().split(" ");
-        System.out.println(Arrays.toString(nums));
-        for (String num : nums) {
-//            if (!num.equals("")) {
-            sum += Integer.parseInt(num);
-//            }
+        String newStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                newStr += str.charAt(i);
+            } else {
+                newStr += " ";
+            }
+        }
+        System.out.println(newStr);
+        newStr = newStr.trim();
+        String[] arrStr = newStr.split(" ");
+        System.out.println(Arrays.toString(arrStr));
+        for (String s : arrStr) {
+            if (!s.equals("")) {
+                sum += Integer.parseInt(s);
+            }
         }
         System.out.println(sum);
+
+//****************************************************************
+
+//        // Tatsiana solution
+//        String s = "aa10aa%^^$#%^$a10ss20bb30aww1tt1";
+//        int sum = 0;
+//        String[] numbers = s.split("[^0-9]+");
+//        for (String number : numbers) {
+//            sum += Integer.parseInt(number);
+//        }
+//        System.out.println("Sum of numbers: " + sum);
+
+//****************************************************************
+
+//        // MaxMish solution
+//        String str = "aa10aa%^^$#%^$a10ss20bb30aww1tt1";
+//        String digits;
+//
+//        digits = str.replaceAll("[^0-9]+"," ");
+//
+//        int sum = 0;
+//        String[] nums = digits.trim().split(" ");
+////        System.out.println(Arrays.toString(nums));
+//        for (String num : nums) {
+//            if (!num.equals("")) {
+//            sum += Integer.parseInt(num);
+//            }
+//        }
+//        System.out.println(sum);
+
 
     }//Main
 }//Class
