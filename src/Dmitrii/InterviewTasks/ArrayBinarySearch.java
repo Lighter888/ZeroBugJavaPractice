857424e180cd4c7e3813a68266ee31ef870c5746
alenb <dmitriiblum@gmail.com>
2023-06-08 18:32:00 -0400

    Array Binary Search added

--- src/Dmitrii/InterviewTasks/ArrayBinarySearch.java
+++ src/Dmitrii/InterviewTasks/ArrayBinarySearch.java
@@ -5,11 +5,6 @@ import java.util.Arrays;
 
 public class ArrayBinarySearch {
     public static void main(String[] args) {
-        Integer[] numbers = {1, 3, 5, 9, 2, 12, 4};
-        System.out.println("Index number of an Array is: " + binarySearch(numbers, 5));
-
-        Character[] chars = {'d', 's', 'k', 'a', 'm'};
-        System.out.println("Index character of an Array is: " + binarySearch(chars, 'm'));
 
         int arr[] = {50,10,40,30,20};
         int key = 30;
@@ -17,16 +12,12 @@ public class ArrayBinarySearch {
         int last=arr.length-1;
         binarySearch(arr, first, last, key);
 
-    }
+        Integer[] numbers = {1, 3, 5, 9, 2, 12, 4};
+        System.out.println("Index number of an Array is: " + binarySearch(numbers, 5));
 
-    // Also we can use ArrayList and Collections class ---> Collections.binarySearch
-    public static int binarySearch(Object[] arrKey, Object indexKey) {
-        // Before use binary search we should to sort an Array
-        Arrays.sort(arrKey);
-        System.out.println(Arrays.toString(arrKey));
-        // Binary Search return the index of search element in Array
-        int index = Arrays.binarySearch(arrKey, indexKey);
-        return index;
+        Character[] chars = {'d', 's', 'k', 'a', 'm'};
+        System.out.println("Index character of an Array is: " + binarySearch(chars, 'm'));
+
     }
 
     // Example of Binary Search
@@ -48,5 +39,15 @@ public class ArrayBinarySearch {
             System.out.println("Element is not found!");
         }
     }
-
+//*************************************************************************************
+    // Also we can use ArrayList and Collections class ---> Collections.binarySearch
+    // It is prepared methods from java developers
+    public static int binarySearch(Object[] arrKey, Object indexKey) {
+        // Before use binary search we should to sort an Array
+        Arrays.sort(arrKey);
+        System.out.println(Arrays.toString(arrKey));
+        // Binary Search return the index of search element in Array
+        int index = Arrays.binarySearch(arrKey, indexKey);
+        return index;
+    }
 }
