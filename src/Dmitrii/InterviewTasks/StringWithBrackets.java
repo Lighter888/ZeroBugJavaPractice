889c1ec3a06d9ca442e1efa592336507c597bd6d
alenb <dmitriiblum@gmail.com>
2023-06-10 20:33:59 -0400

    Array Binary Search added

---
+++ src/Dmitrii/InterviewTasks/StringWithBrackets.java
@@ -0,0 +1,25 @@
+package Dmitrii.InterviewTasks;
+
+public class StringWithBrackets {
+
+
+    public static void main(String[] args) {
+        String str = "{{{{}()[[]]}}}";
+        System.out.println(task(str));
+    }
+    public static boolean task (String str){
+
+        int countL = 0;
+        int countR = 0;
+
+        for(int i =0; i<str.length(); i++){
+            if(str.charAt(i)=='{'){
+                countL++;
+            }
+            if(str.charAt(i)=='}'){
+                countR++;
+            }
+        }
+      return countL == countR;
+    }
+}
