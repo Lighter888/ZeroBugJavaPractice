8d8db94db9240d1c16583d99d10cead8101f27bf
alenb <dmitriiblum@gmail.com>
2023-06-21 10:56:18 -0400

    New solution of is Armstrong Numbers added

---
+++ src/Dmitrii/InterviewTasks/Armstrong.java
@@ -0,0 +1,23 @@
+package Dmitrii.InterviewTasks;
+
+public class Armstrong {
+    public static void main (String[] args){
+        int num1 = 370;
+        int num2 = 587;
+        System.out.println("Number "+ num1 +" is Armstrong: " + isArmstrong(num1));
+        System.out.println("Number "+ num2 +" is Armstrong: " + isArmstrong(num2));
+    }
+    public static boolean isArmstrong(int num){
+        int temp = 0;
+        int digit = 0;
+        int sum = 0;
+
+        temp = num;
+        while(temp>0){
+            digit = temp%10;
+            sum = sum + (digit * digit * digit);
+            temp = temp/10;
+        }
+        return num == sum;
+    }
+}
