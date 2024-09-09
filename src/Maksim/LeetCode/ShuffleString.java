d58b394ac9f552b602bce29aa605600bcf74f5d2
Max Mish <mishcenkomv@gmail.com>
2024-09-09 16:23:32 -0400

    add shuffle string task

--- src/Maksim/LeetCode/ShuffleString.java
+++ src/Maksim/LeetCode/ShuffleString.java
@@ -7,7 +7,7 @@ public class ShuffleString {
 //https://leetcode.com/problems/shuffle-string/
 
     static String restoreString(String s, int[] indices) {
-
+    
         String res = "";
         String[] orig = s.split("");
         System.out.println(Arrays.toString(orig));
@@ -16,6 +16,12 @@ public class ShuffleString {
             res += orig[index];
         }
         return res;
+    }
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String str1 = String.join("", word1);
+        String str2 = String.join("", word2);
+        return str1.equals(str2);
     }
 
     public static void main(String[] args) {
---
+++ src/Maksim/TryCatch.java
@@ -0,0 +1,25 @@
+package Maksim;
+
+public class TryCatch {
+    public static void main(String[] args) throws Exception {
+        try {
+            System.out.println("A");
+            System.out.println(hello());
+            System.out.println("B");
+        } catch (Exception e) {
+            System.out.println("C");
+        }
+    }
+
+    public static String hello() throws Exception {
+        try {
+            System.out.println("1");
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println("2");
+            return "X";
+        } finally {
+            System.out.println("3");
+        }
+    }
+}
